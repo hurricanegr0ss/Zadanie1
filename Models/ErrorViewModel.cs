954bee98385795e1d73d1d52c2c943e738c762b4
hurricanegr0ss <hurricanegr0ss987@gmail.com>
2023-03-15 14:50:47 +0100

    git initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt1_MG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
